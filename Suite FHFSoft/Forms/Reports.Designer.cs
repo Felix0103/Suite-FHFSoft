cab96b5bfbb863261e34dde84a953f406b4827bd
Felix Hernandez <datahernandez@gmail.com>
2017-09-13 17:52:12 -0400

    Felix Trabajo

---
+++ Suite FHFSoft/Forms/Reports.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Suite_FHFSoft
+{
+    partial class Reports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.radTitleBar1 = new Telerik.WinControls.UI.RadTitleBar();
+            ((System.ComponentModel.ISupportInitialize)(this.radTitleBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(3, 32);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ShowLogo = false;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(740, 493);
+            this.crystalReportViewer1.TabIndex = 2;
+            this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // radTitleBar1
+            // 
+            this.radTitleBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radTitleBar1.Location = new System.Drawing.Point(0, 0);
+            this.radTitleBar1.Name = "radTitleBar1";
+            this.radTitleBar1.Size = new System.Drawing.Size(765, 35);
+            this.radTitleBar1.TabIndex = 3;
+            this.radTitleBar1.TabStop = false;
+            this.radTitleBar1.Text = "Reports";
+            this.radTitleBar1.Click += new System.EventHandler(this.radTitleBar1_Click);
+            // 
+            // Reports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(765, 537);
+            this.Controls.Add(this.radTitleBar1);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Reports";
+            this.Text = "Reports";
+            this.Load += new System.EventHandler(this.Reports_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radTitleBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private Telerik.WinControls.UI.RadTitleBar radTitleBar1;
+    }
+}
---
+++ Suite FHFSoft/Forms/Reports.cs
@@ -0,0 +1,60 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suite_FHFSoft
+{
+    public partial class Reports : Form
+    {
+        ReportDocument report = new ReportDocument();
+
+        DataTable dtReport = new DataTable();
+        public string vReportName = "";
+        public string vSQLString = "";
+
+        string vTitleReports = "";
+        public Reports()
+        {
+            InitializeComponent();
+        }
+
+        private void radTitleBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Reports_Load(object sender, EventArgs e)
+        {
+            LoadReport();
+        }
+
+        private void LoadReport()
+        {
+
+            dtReport = C.SQL(vSQLString);
+
+
+
+            report.Load("C:\\Users\\LHVCDevelopment1\\Documents\\Visual Studio 2015\\Projects\\Evolution\\Evolution\\Reports\\" + vReportName);
+            report.SetDatabaseLogon("", "");
+            report.SetDataSource(dtReport);
+
+            crystalReportViewer1.ReportSource = report;
+            //crystalReportViewer1.ReportSource= "C:\\Users\\LHVCDevelopment1\\Documents\\Visual Studio 2015\\Projects\\Evolution\\Evolution\\Reports\\" + vReportName;
+
+            crystalReportViewer1.Refresh();
+        }
+
+        private void SetTitle()
+        {
+            this.Text = vTitleReports;
+        }
+    }
+}
